676af62b9894bb41146f7e34c743b05bd2f841b8
dvno-v <hypnotizedstefan23@gmail.com>
2021-03-20 14:34:18 +0200

    Python 101 forever

---
+++ Python101-Hack-Bulgaria/test.py
@@ -0,0 +1,25 @@
+a_string = "tova e string"
+
+print(a_string)
+
+if a_string == "tova e string":
+    print('dobre e')
+print('not in if')
+    
+class Main: 
+    def __init__ (self):
+        self.a = 15
+
+a = Main()
+
+print(a.a);
+
+for i in range(20):
+    print(i)
+
+if True:
+    print("this is true")
+elif True:
+    print("this is sparta")
+else:
+    pass
